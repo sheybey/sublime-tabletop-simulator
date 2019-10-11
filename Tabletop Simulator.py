f352668ad848db3fe51ad00546fa5cbcc322f567
Sam Heybey <sam@heybey.org>
2019-10-11 13:33:44 -0400

    cleanup
     - remove pointless Global "class"
     - always start the socket server
     - switch to single-threaded socket server implementation
     - remove start/stop server commands
     - show a message for unhandled TTS messages

--- Tabletop Simulator.py
+++ Tabletop Simulator.py
@@ -5,19 +5,16 @@ import socketserver
 import threading
 import json
 
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer): pass
+views = {}
 
-class Global:
-    server = None
-    views = {}
 
 def open_script(script, window):
-    view = Global.views.get(script["guid"], None)
+    view = views.get(script["guid"], None)
     if view is not None:
         view.run_command("erase_buffer")
     else:
         view = window.new_file()
-        Global.views[script["guid"]] = view
+        views[script["guid"]] = view
         view.set_syntax_file("Packages/Lua/Lua.sublime-syntax")
     if script["guid"] != "-1":
         view.set_name(script["name"] + " - " + script["guid"])
@@ -25,6 +22,7 @@ def open_script(script, window):
         view.set_name(script["name"])
     view.run_command("append_to_buffer", {"text": script["script"]})
     view.window().focus_view(view)
+
 
 class EditorAPIHandler(socketserver.StreamRequestHandler):
     def handle(self):
@@ -40,7 +38,7 @@ class EditorAPIHandler(socketserver.StreamRequestHandler):
             window.run_command("show_panel", {"panel": "output.tts_messages"})
 
         elif data["messageID"] == 3:
-            view = Global.views.get(data["guid"], None)
+            view = views.get(data["guid"], None)
             if view is None:
                 view = window.active_view()
             else:
@@ -50,29 +48,42 @@ class EditorAPIHandler(socketserver.StreamRequestHandler):
                 "text": data["errorMessagePrefix"] + data["error"] + "\n"
             })
             window.run_command("show_panel", {"panel": "output.tts_messages"})
+        else:
+            window.create_output_panel('tts_messages').run_command('append_to_buffer', {
+                'text': 'unhandled message:\n' + repr(data) + '\n'
+            })
+
+
+server = socketserver.TCPServer(("localhost", 39998), EditorAPIHandler, False)
+
 
 def start_server():
-    if Global.server is not None: return
-    server = ThreadedTCPServer(("localhost", 39998), EditorAPIHandler)
-    Global.server = server
-    threading.Thread(target=server.serve_forever).start()
+    server.server_bind()
+    server.server_activate()
+    server.serve_forever()
 
-def stop_server():
-    if Global.server is None: return
-    Global.server.shutdown()
-    Global.server.server_close()
-    Global.server = None
 
-#def plugin_loaded(): start_server()
-def plugin_unloaded(): stop_server()
+def plugin_loaded():
+    server_thread = threading.Thread(target=start_server)
+    server_thread.daemon = True
+    server_thread.start()
+
+
+def plugin_unloaded():
+    server.shutdown()
+    server.close_server()
+
 
 class EraseBufferCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if self.view.size() != 0: self.view.erase(edit, sublime.Region(0, self.view.size()))
+        if self.view.size() != 0:
+            self.view.erase(edit, sublime.Region(0, self.view.size()))
+
 
 class AppendToBufferCommand(sublime_plugin.TextCommand):
     def run(self, edit, text = ""):
         self.view.insert(edit, self.view.size(), text)
+
 
 def send_data(data):
     view = sublime.active_window().active_view()
@@ -94,36 +105,31 @@ def send_data(data):
         view.set_status("z_tts_error", "Unable to connect to Tabletop Simulator: Connection refused")
         return None
 
+
 class GetScriptsCommand(sublime_plugin.WindowCommand):
     def run(self):
         scripts = send_data({"messageID": 0})
         if scripts is not None:
             for script in scripts["scriptStates"]: open_script(script, self.window)
 
+
 class SendScriptsCommand(sublime_plugin.WindowCommand):
     def run(self):
         scripts = []
-        for guid in Global.views.keys():
+        for guid in views.keys():
             scripts.append({
                 "guid": guid,
-                "script": Global.views[guid].substr(
-                    sublime.Region(0, Global.views[guid].size())
+                "script": views[guid].substr(
+                    sublime.Region(0, views[guid].size())
                 )
             })
         send_data({"messageID": 1, "scriptStates": scripts})
-    def is_enabled(self): return len(Global.views) > 0
+    def is_enabled(self): return len(views) > 0
 
-class StartServerCommand(sublime_plugin.ApplicationCommand):
-    def run(self): start_server()
-    def is_enabled(self): return Global.server is None
-
-class StopServerCommand(sublime_plugin.ApplicationCommand):
-    def run(self): stop_server()
-    def is_enabled(self): return Global.server is not None
 
 class CleanUpViews(sublime_plugin.EventListener):
     def on_close(self, view):
-        for guid in Global.views.keys():
-            if view == Global.views[guid]:
-                del Global.views[guid]
+        for guid in views.keys():
+            if view == views[guid]:
+                del views[guid]
                 return
